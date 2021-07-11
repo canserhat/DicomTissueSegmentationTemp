5680deec4bd2f5a48f85efe2c16d9949e3ab8b53
Serhat Can <canserhat91@gmail.com>
2021-07-11 23:41:45 +0300

    Update AKVeinBinaryThinningImageFilter.h

--- AKVeinBinaryThinningImageFilter.h
+++ AKVeinBinaryThinningImageFilter.h
@@ -2,10 +2,10 @@
 #define AKVEINBINARYTHINNINGIMAGEFILTER_H
 
 
+
 #include "QThread"
 #include "QObject"
 #include "QEventLoop"
-
 #include <itkNeighborhoodIterator.h>
 #include <itkImageToImageFilter.h>
 #include <itkImageRegionIteratorWithIndex.h>
@@ -15,6 +15,19 @@
 
 #include "AKImageToPolygon/AKGlobalPolygonOperationManagement.h"
 
+
+/** This filter is a parallel thinning algorithm and is an implementation
+* of the algorithm described in:
+*
+* T.C. Lee, R.L. Kashyap, and C.N. Chu.
+* Building skeleton models via 3-D medial surface/axis thinning algorithms.
+* Computer Vision, Graphics, and Image Processing, 56(6):462--478, 1994.
+*
+* To do: Make use of multi-threading.
+*
+* \author Hanno Homann, Oxford University, Wolfson Medical Vision Lab, UK.
+*/
+
 class AKVeinBinaryThinningImageFilter:public QThread
 {    
 
@@ -23,7 +36,7 @@ public:
     /** @brief
      Implementation of extraction of vein skeleton structure
      */
-    
+
     AKVeinBinaryThinningImageFilter(GlobalMaskImageType::Pointer _inputBinaryImage,
                                     GlobalMaskImageType::RegionType _boundingBoxImage);
 
@@ -41,32 +54,29 @@ private:
 
     GlobalMaskImageType::RegionType boundingBoxImage;
     GlobalMaskImageType::Pointer inputBinaryImage;
-    
+
 
     void runThinning();
     void PrepareData();
     void ComputeThinImage();
-    
+
      /** Check for Euler invariance (see [Lee94]). */
     bool IsEulerInvariant(NeighborhoodIteratorType::NeighborhoodType neighbors, const int * LUT);
-    
+
     /** Fill the Euler look-up table (LUT) for later check of the Euler
      * invariance (see [Lee94]). */
     void FillEulerLUT(int * LUT);
-    
+
     /** Check if the current point is a simple point.
       * This method is named 'N(v)_labeling' in [Lee94].
       * Outputs the number of connected objects in a neighborhood of a point
       * after this point would have been removed. */
     bool IsSimplePoint(NeighborhoodIteratorType::NeighborhoodType neighbors);
-    
+
     /** Recursive method that calculates the number of connected components in
       * the 3D neighbourhood after the center pixel would have been removed (see)
       * [Lee94]). */
     void OctreeLabeling(int octant, int label, int * cube);
-
-
-    
 
 
 };
